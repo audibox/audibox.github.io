194362d07428a6ff4d50bcf95ff61d9dc8bf8720
Thien Vu <53557817+devvau6868@users.noreply.github.com>
2020-07-14 00:17:22 +0700

    'back-to-top'

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,35 +1,94 @@
 jQuery(document).ready(function($){
 
-    // Smooth on external page
-    $(function() {
-      setTimeout(function() {
-        if (location.hash) {
-          /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
-          window.scrollTo(0, 0);
-          target = location.hash.split('#');
-          smoothScrollTo($('#'+target[1]));
-        }
-      }, 1);
+    // // Smooth on external page
+    // $(function() {
+    //   setTimeout(function() {
+    //     if (location.hash) {
+    //       /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
+    //       window.scrollTo(0, 0);
+    //       target = location.hash.split('#');
+    //       smoothScrollTo($('#'+target[1]));
+    //     }
+    //   }, 1);
 
-      // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-      $('a[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          smoothScrollTo($(this.hash));
-          return false;
-        }
-      });
+    //   // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    //   $('a[href*=\\#]:not([href=\\#])').click(function() {
+    //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //       smoothScrollTo($(this.hash));
+    //       return false;
+    //     }
+    //   });
 
-      function smoothScrollTo(target) {
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //   function smoothScrollTo(target) {
+    //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-        }
-      }
-    });
+    //     if (target.length) {
+    //       $('html,body').animate({
+    //         scrollTop: target.offset().top
+    //       }, 1000);
+    //     }
+    //   }
+    // });
+
+
+
+/* Please ❤ this if you like it! */
+
+// Scroll progress
+
+// (function($) { "use strict";
+
+	//Switch dark/light
 	
+	// $(".switch").on('click', function () {
+	// 	if ($("body").hasClass("light")) {
+	// 		$("body").removeClass("light");
+	// 		$(".switch").removeClass("switched");
+	// 	}
+	// 	else {
+	// 		$("body").addClass("light");
+	// 		$(".switch").addClass("switched");
+	// 	}
+	// });
+		
+	$(document).ready(function(){"use strict";
+	
+		//Scroll back to top
+		
+		var progressPath = document.querySelector('.progress-wrap path');
+		var pathLength = progressPath.getTotalLength();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+		progressPath.style.strokeDashoffset = pathLength;
+		progressPath.getBoundingClientRect();
+		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+		var updateProgress = function () {
+			var scroll = $(window).scrollTop();
+			var height = $(document).height() - $(window).height();
+			var progress = pathLength - (scroll * pathLength / height);
+			progressPath.style.strokeDashoffset = progress;
+		}
+		updateProgress();
+		$(window).scroll(updateProgress);	
+		var offset = 50;
+		var duration = 550;
+		jQuery(window).on('scroll', function() {
+			if (jQuery(this).scrollTop() > offset) {
+				jQuery('.progress-wrap').addClass('active-progress');
+			} else {
+				jQuery('.progress-wrap').removeClass('active-progress');
+			}
+		});				
+		jQuery('.progress-wrap').on('click', function(event) {
+			event.preventDefault();
+			jQuery('html, body').animate({scrollTop: 0}, duration);
+			return false;
+		})
+		
+		
+	});
+	
+// })(jQuery); 
 	
 	// toggle comments
     $('.show-comments').on('click', function() {  
@@ -47,6 +106,8 @@ jQuery(document).ready(function($){
      });
     
  });   
+
+
 
 // deferred style loading
 var loadDeferredStyles = function () {
